a805181151d36e33c6df13ae60449ca437d72868
cmidgley <chris@Koose.com>
2024-06-16 14:04:28 -0400

    commit

---
+++ src/mymodule/esp32/example.ts
@@ -0,0 +1,5 @@
+import { shared } from '../shared.js';
+
+export function example () { 
+    trace(`Inside example function, shared is ${shared()}\n`);
+}
---
+++ src/mymodule/node/example.ts
@@ -0,0 +1,5 @@
+import { shared } from '../shared.js';
+
+export function example () { 
+    console.log(`Inside example function, shared is ${shared()}`);
+}
